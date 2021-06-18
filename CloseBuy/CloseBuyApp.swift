d0b91755abc5ad7975e08c4a101e6f7e5864bc3d
Connor Lynch <connorepic123@gmail.com>
2021-06-18 14:17:48 +0100

    Initial Commit

---
+++ CloseBuy/CloseBuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloseBuyApp.swift
+//  CloseBuy
+//
+//  Created by Connor A Lynch on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct CloseBuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
