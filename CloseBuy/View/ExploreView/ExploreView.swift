a3920d6764e9179968e5fe2a7cbde15f950762a2
k.siddhartha <k.siddhartha.varma@gmail.com>
2021-07-06 22:34:57 +0530

    Added ExploreUI folder

---
+++ CloseBuy/View/ExploreView/ExploreView.swift
@@ -0,0 +1,20 @@
+//
+//  ExploreView.swift
+//  CloseBuy
+//
+//  Created by k.siddhartha varma on 06/07/21.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ExploreView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExploreView()
+    }
+}
